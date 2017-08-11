7c2a2e792ee88979a5f991610376c930fd814a6d
Rohan Jansen <rohan@flatcircle.io>
2017-08-11 16:06:34 +0200

    Getting the initial thought processes going

--- Example/PopKit/ViewController.swift
+++ Example/PopKit/ViewController.swift
@@ -12,13 +12,61 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        _ = PopKitBuilder() {
+            $0.backgroundEffect = .blurDark
+            $0.inAnimation = .fromBottom
+            $0.outAnimation = .fromTop
+            $0.popupView = TestView()
+            $0.mainAction = { print("Did perform main action") }
+            $0.dismissAction = { print("Did perform dimiss action") }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
+
+class TestView: UIView, PopView {
+    
+}
+
+
+enum PopKitAnimation {
+    case zoomIn(Float)
+    case zoomOut(Float)
+    case fromTop
+    case fromLeft
+    case fromRight
+    case fromBottom
+}
+
+enum PopKitBackgroundEffect {
+    case blurLight
+    case blurDark
+    case transparentOverlay(Float)
+}
+
+internal func PopKitBuilder(_ builder: (PopKit) -> Void) -> PopKit {
+    let kit = PopKit()
+    builder(kit)
+    return kit
+}
+
+protocol PopView { }
+
+extension PopView where Self: UIView {
+    
+    func dismiss() {
+        
+    }
+}
+
+class PopKit {
+    var popupView: PopView?
+    var dismissAction: (() -> Void)?
+    var mainAction: (() -> Void)?
+    var inAnimation: PopKitAnimation = .fromTop
+    var outAnimation: PopKitAnimation = .fromBottom
+    var backgroundEffect: PopKitBackgroundEffect = .blurLight
+}
+
+
 
